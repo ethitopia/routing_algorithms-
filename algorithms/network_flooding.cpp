b42cf6852c502e3fd727cd5e271faf8ee0727f2b
Jupiter Research <ethanma1000@gmail.com>
2025-08-04 15:15:19 -0400

    Network flooding implemented  - testing

--- algorithms/network_flooding.cpp
+++ algorithms/network_flooding.cpp
@@ -1,11 +1,32 @@
 #include <fstream>
 #include <iostream>
+#include <set>
+#include <queue>
 #include <list>
 #include <unordered_map>
 
 // 2-E network flooding
 void networkFlooding(std::unordered_map<int, std::list<std::pair<int, int>>> graph, int source)
 {
-	// access all pairs
-	return;
-}
+	std::set<int> visited; 
+	std::queue<int> queue; 
+
+	std::cout << "Starting flood from node:" << source << std::endl; 
+
+	queue.push(source); 
+	visited.insert(source); 
+
+	while (!queue.empty()) {
+		int node = queue.pop(); 
+		std::cout << "Flooding has reached node: " << node << std::endl; 
+
+		for (const auto& neigh : graph[node]) {
+			if (visited.find(neigh)){
+				visited.insert(neigh)
+				queue.push(neigh); 
+			}
+		}
+		}
+
+
+	}
